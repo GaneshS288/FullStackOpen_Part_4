7c8b9ae69453a5a03462348310ef5473a9ffa0c7
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-14 18:19:55 +0530

    feat: add error handler to app

--- blog_list/app.js
+++ blog_list/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import blogRouter from "./routes/blogRouter.js";
+import { errorHandler } from "./util/middleware.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
+
+app.use(errorHandler);
 
 export default app;
 
