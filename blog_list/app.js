7ff39d328ca50548e8beb39c4857e7f412af0ce2
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-15 21:18:46 +0530

    feat: add request logger to app

--- blog_list/app.js
+++ blog_list/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import blogRouter from "./routes/blogRouter.js";
 import { errorHandler } from "./util/middleware.js";
+import { request as requestLogger } from "./util/logger.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use("/api/blogs", blogRouter);
 
--- blog_list/util/logger.js
+++ blog_list/util/logger.js
@@ -1,5 +1,9 @@
-function request(req) {
-    console.log(req);
+function request(req, res, next) {
+    const {method, path, body } = req;
+
+    console.log(`Method: ${method}, Path: ${path}`);
+    console.log(`Body: ${JSON.stringify(body)}`);
+    next()
 }
 
 function error(error) {
