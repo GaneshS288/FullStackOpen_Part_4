d0ac1fc3fbbf5c2b5cb3e4f0ddfdfa084fe3f987
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-15 16:22:48 +0530

    feat: write most blogs function and its tests

--- blog_list/util/list_helper.js
+++ blog_list/util/list_helper.js
@@ -3,18 +3,17 @@ function dummy(blogs) {
 }
 
 function totalLikes(blogs) {
-    if(blogs.length === 0) {
+    if (blogs.length === 0) {
         return 0;
     }
 
-    const total = blogs.reduce((accu, next) => accu + next.likes, 0);
+    const total = blogs.reduce((accu, curr) => accu + curr.likes, 0);
 
     return total;
 }
 
 function favoriteBlog(blogs) {
-    if(blogs.length === 0) 
-        return null;
+    if (blogs.length === 0) return null;
 
     //descending sort
     const sortedBlogs = blogs.toSorted((a, b) => b.likes - a.likes);
@@ -22,4 +21,28 @@ function favoriteBlog(blogs) {
     return sortedBlogs[0];
 }
 
-export { dummy, totalLikes, favoriteBlog };
+function mostBlogs(blogs) {
+    if(blogs.length === 0)
+        return null;
+
+    const authorsWithBlogCount = blogs.reduce((accu, curr) => {
+        const authorAlreadyInAccu = accu.find((author) => {
+            return author.author === curr.author;
+        });
+
+        if (authorAlreadyInAccu) authorAlreadyInAccu.blogs += 1;
+        else if (!authorAlreadyInAccu)
+            accu.push({ author: curr.author, blogs: 1 });
+
+        return accu;
+    }, []);
+
+    //sort authors in descending order
+    const sortedAuthors = authorsWithBlogCount.sort(
+        (a, b) => b.blogs - a.blogs
+    );
+
+    return sortedAuthors[0];
+}
+
+export { dummy, totalLikes, favoriteBlog, mostBlogs };
