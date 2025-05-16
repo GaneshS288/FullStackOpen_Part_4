8c517a7d2d8a8dd6f82f85b97abcff1b5b5cf523
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-16 18:13:34 +0530

    feat: add test for confirming returned blogs have id property

--- blog_list/tests/blog_api.test.js
+++ blog_list/tests/blog_api.test.js
@@ -1,26 +1,39 @@
-import {describe ,beforeEach, test, after} from "node:test";
+import { describe, beforeEach, test, after } from "node:test";
 import { strictEqual } from "node:assert";
 import supertest from "supertest";
 import app from "../app.js";
 import Blog, { mongoose } from "../models/blog.js";
 import { testBlogsData } from "./testHelper.js";
 
+const api = supertest(app);
+
 describe("Blog api", () => {
     beforeEach(async () => {
         await Blog.deleteMany({});
         await Blog.insertMany(testBlogsData);
-    })
+    });
 
     test("get request returns correct number of blogs", async () => {
-        const api = supertest(app);
+        const res = await api
+            .get("/api/blogs")
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
 
-       const res = await api.get("/api/blogs").expect(200).expect("Content-Type", /application\/json/);
+        strictEqual(res.body.length, 6);
+    });
 
-       strictEqual(res.body.length, 6);
-    })
-})
+    test("returned blogs have id parameter", async () => {
+        const { body } = await api
+            .get("/api/blogs")
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
 
-after( async () => {
+        const hasIdInEveryBlog = body.every((blog) => blog.id);
+
+        strictEqual(hasIdInEveryBlog, true);
+    });
+});
+
+after(async () => {
     await mongoose.connection.close();
-})
-
+});
